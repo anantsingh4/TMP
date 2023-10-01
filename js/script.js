0656d4813bc9ae0b28c07e5e275026cddfdc6298
anantsingh4 <31586340+anantsingh4@users.noreply.github.com>
2023-10-02 02:10:51 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal('.text', {delay:250, origin:'left'});
+sr.reveal('.Events', {delay:300, origin:'right'});
+sr.reveal('.coordinators', {delay:350, origin:'left'});
+sr.reveal('.abcd', {delay:400, origin:'bottom'});
